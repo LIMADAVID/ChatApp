8b5e4e50535ad2cee2a701308ff0a1b78470bba8
LIMADAVID <davidlima.dev@gmail.com>
2020-12-07 22:00:14 -0300

    Finalização do projeto

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,50 +1,49 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, TouchableOpacity, Platform } from 'react-native';
-import { Item, Input as InputNB, Icon } from 'native-base'; 
+import { Item, Input as InputNB, Icon } from 'native-base';
 
 export default function Input(props) {
-    const { sendMessage } = props;
-    const [message, setMessage] = useState('');
+  const { sendMessage } = props;
+  const [message, setMessage] = useState('');
 
-    const onSubmit = () => {
-        if(message.length > 0) {
-            sendMessage(message);
-            setMessage('');
-        }
+  const onSubmit = () => {
+    if(message.length > 0) {
+      sendMessage(message);
+      setMessage('');
     }
+  }
 
-    return (
-        <View styles={styles.container}>
-            <Item styles={styles.itemInput}>
-                <InputNB 
-                    placeholder='Mensagem...' 
-                    style={styles.input} 
-                    placeholderTextColor='grey'  
-                    value={message}
-                    onChange={(e) => setMessage(e.nativeEvent.text)}                  
-                />
-                <TouchableOpacity style={{paddingRight: 5}} onPress={onSubmit}>
-                    <Icon name='send' style={styles.iconSend} />
-                </TouchableOpacity>
-            </Item>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Item style={styles.itemInput}>
+        <InputNB
+          placeholder='Mensagem...'
+          style={styles.input}
+          placeholderTextColor='grey'
+          value={message}
+          onChange={(e) => setMessage(e.nativeEvent.text)}
+        />
+        <TouchableOpacity style={{paddingRight: 5}} onPress={onSubmit}>
+          <Icon name='send' style={styles.iconSend} />
+        </TouchableOpacity>
+      </Item>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#16202b',
-        paddingBottom: 20,
-        paddingHorizontal: 20
-    },
-    itemInput: {
-        borderColor: '#16202b'        
-              
-    },
-    input: {
-        color: '#fff'        
-    },
-    iconSend: {
-       color: '#fff'
-    }
+  container: {
+    backgroundColor: '#16202b',
+    paddingBottom: Platform.OS === 'ios' ? 20 : 0,
+    paddingHorizontal: 20
+  },
+  itemInput: {
+    borderColor: '#16202b'
+  },
+  input: {
+    color: '#fff'
+  },
+  iconSend: {
+    color: '#fff'
+  }
 })
--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,34 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Header, Body, Title } from 'native-base';
 import moment from 'moment';
+import { map } from 'lodash';
 import Input from '../components/Input';
+import Message from '../components/Message';
 import firebase from '../utils/firebase';
 import 'firebase/database';
 
 export default function Chat(props) {
-    const {userName} = props;
-    const sendMessage = (message) => {
-        const time = moment().format('hh:mm a')
-        
-        firebase
-            .database()
-            .ref('general')
-            .push({ userName, text: message, time })
-    }
+  const { userName } = props;
+  const [messages, setMessages] = useState([]);
+  const chatScrollRef = useRef();
 
-    return (
-        <>
-            <View style={styles.content}>
-                <Text>CHAT</Text> 
-                <Input  sendMessage={sendMessage} />
-            </View>
-        </>
-    )
+  useEffect(() => {
+    const chat = firebase.database().ref('general');
+    chat.on('value', (snapshot) => {
+      setMessages(snapshot.val());
+    });
+  }, []);
+
+  useEffect(() => {
+    chatScrollRef.current.scrollTo({ y: 10000000000000 });
+  }, [messages]);
+
+  const sendMessage = (message) => {
+    const time = moment().format('hh:mm a');
+    
+    firebase.database()
+        .ref('general')
+        .push({ userName, text: message, time });
+  };
+
+  return (
+    <>
+      <Header style={styles.header} iosBarStyle="light-content">
+        <Body>
+          <Title style={{ color: '#fff' }}>Chat App</Title>
+        </Body>
+      </Header>
+      <View style={styles.content}>
+        <ScrollView style={styles.chatView} ref={chatScrollRef}>
+          {map(messages, (message, index) => (
+            <Message key={index} message={message} name={userName} />
+          ))}
+        </ScrollView>
+        <Input sendMessage={sendMessage} />
+      </View>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-    content: {
-        flex: 1,
-        justifyContent: 'space-between'
-    }
+  header: {
+    backgroundColor: '#16202b'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  chatView: {
+    backgroundColor: '#1b2734'
+  }
 })
